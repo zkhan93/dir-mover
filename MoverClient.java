8e71afb9130418dd8ef6a21bd179ccc111430c5a
Zeeshan Khan <zkhan1093@gmail.com>
2016-02-12 12:29:02 +0530

    receving correctly

--- MoverClient.java
+++ MoverClient.java
@@ -3,7 +3,9 @@ import java.net.*;
 class MoverClient{
 	public static String SAVE_FILENAME="received";
 	public static int BUFFER_SIZE=16*1024;
-	public static int B_TO_MB=1024*1024;
+	public static int B_TO_KB=1024;
+	public static int B_TO_MB=B_TO_KB*1024;
+	public static int B_TO_GB=B_TO_MB*1024;
 	public static int PORT=8932;
 	public static void main(String args[]){
 		String serverIp=null;
@@ -15,10 +17,10 @@ class MoverClient{
 		if(serverIp!=null){
 			Socket socket=connectToServer(serverIp);
 			if(socket!=null)
-				printData(socket);
+				saveDir(socket);
 		}
 	}
-	public static void printData(Socket socket){
+	public static void saveDir(Socket socket){
 		try{
 			DataInputStream in=new DataInputStream(new BufferedInputStream(socket.getInputStream()));
 			String newFilename=null;
@@ -32,26 +34,36 @@ class MoverClient{
 			for(int i=0;i<totalFiles;i++){
 				newFilename=in.readUTF();
 				newFileSize=in.readLong();
-				System.out.println(rootFile+" "+newFilename);
 				newFile=new File(rootFile.getCanonicalPath(),newFilename);
-				System.out.println(newFile);
-				if(newFile.getParentFile().mkdir()){
-					newFile.createNewFile();
-					OutputStream out=new FileOutputStream(newFile);
-					byte[] buffer=new byte[BUFFER_SIZE];
-					int bytesRead;
-					long toRead=newFileSize;
-					while(toRead>0){
-						bytesRead=in.read(buffer);
-						out.write(buffer,0,bytesRead);
-						toRead-=bytesRead;
+				
+				if(newFile.getParentFile().mkdirs())
+					;//System.out.println("created directories for file"+newFile.getName());
+				else
+					;//System.out.println("Directories already exist for file"+newFile.getName());
+				newFile.createNewFile();
+				OutputStream out=new FileOutputStream(newFile);
+				byte[] buffer=new byte[BUFFER_SIZE];
+				int bytesRead;
+				long toRead=newFileSize;
+				while(toRead>0){
+					bytesRead=in.read(buffer,0,(int)(toRead<BUFFER_SIZE?toRead:BUFFER_SIZE));
+					out.write(buffer,0,bytesRead);
+					toRead-=bytesRead;
+					if(newFileSize<B_TO_KB){
+						System.out.print("receiving : "+newFile.getName()+" : "+String.format("%d Bytes of %d Bytes",newFileSize-toRead,newFileSize));
+					}else if (newFileSize<B_TO_MB){
+						System.out.print("receiving : "+newFile.getName()+" : "+String.format("%.2f KB of %.2f KB",((float)(newFileSize-toRead))/B_TO_KB,((float)newFileSize)/B_TO_KB));
 					}
-					out.flush();
-					out.close();
-					System.out.println("to read "+toRead);
-				}else{
-					System.out.println("cannot create dirs for file");
+					else if(newFileSize<B_TO_GB){
+						System.out.print("receiving : "+newFile.getName()+" : "+String.format("%.2f MB of %.2f MB",((float)(newFileSize-toRead))/B_TO_MB,((float)newFileSize)/B_TO_MB));
+					}else{
+						System.out.print("receiving : "+newFile.getName()+" : "+String.format("%.2f GB of %.2f GB",((float)(newFileSize-toRead))/B_TO_GB,((float)newFileSize)/B_TO_GB));
+					}
 				}
+				out.flush();
+				out.close();
+				System.out.println("");
+				
 			}
 		}catch(Exception ex){
 			ex.printStackTrace();
